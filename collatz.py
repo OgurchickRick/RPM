3838523286fc328a514379151c27b8a91116678f
OgurchickRick <artem.derbin.04@list.ru>
2022-01-25 14:43:19 +0300

    Finish

--- collatz.py
+++ collatz.py
@@ -1,2 +1,29 @@
-while True:
-    print('я разбираюсь с гитхабом')
+x = int(input('Введите число:'))
+A = []
+
+
+def x2(x):
+    x = x / 2
+    return collatz(x)
+
+
+def x3_1(x):
+    x = x * 3 + 1
+    return collatz(x)
+
+
+def collatz(x):
+    global A
+    if x == 1:
+        A.append(int(x))
+        return x
+    elif x % 2 == 0:
+        A.append(int(x))
+        return x2(x)
+    else:
+        A.append(int(x))
+        return x3_1(x)
+
+
+collatz(x)
+print(A)
